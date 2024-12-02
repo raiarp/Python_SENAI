bddc46300132699d61b04db602d270225bb66f5e
Ryan Pereira <ryrape@gmail.com>
2024-12-02 10:00:31 -0300

    Aprendizado sobre o que é uma variável

---
+++ Aula 02/Aula02.py
@@ -0,0 +1,19 @@
+print('life imitates art')
+
+#string(texto str)
+nome_usuario = 'Ryan'
+
+#inteiro(int)
+idade_usuario = 19
+
+#float
+altura_usuario = 1.71
+
+#True or False (booleano - bool)
+maior_de_idade = True
+
+print(nome_usuario)
+print(idade_usuario)
+print(altura_usuario)
+
+print('Seja bem-vindo',nome_usuario, 'sua idade eh', idade_usuario, 'sua altura eh', altura_usuario, 'voce eh maior de idade?', maior_de_idade)
